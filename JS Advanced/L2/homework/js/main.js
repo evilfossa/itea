a408fbcc33d32f09b3d5bd30afb2f7382975a549
Vitalii Kopach <evilfossa@gmail.com>
2019-10-31 16:04:37 +0200

    Done.

--- JS Advanced/L2/homework/js/main.js
+++ JS Advanced/L2/homework/js/main.js
@@ -38,33 +38,53 @@
 
 */
 
-const btn = document.querySelectorAll('button.showButton')
-const div = document.querySelectorAll('div.tab')
-// console.log(btn);
-// console.log(div);
+// Первый вариант. Рабочий, но не прикольный.
 
-btn[0].onclick = function (event) {
-  // console.log("Tab 1");
-  // console.log(btn[0].dataset.tab);
-  div[0].classList.toggle('active');
-}
+// const btn = document.querySelectorAll('button.showButton')
+// const div = document.querySelectorAll('div.tab')
+// // console.log(btn);
+// // console.log(div);
 
-btn[1].onclick = function (event) {
-  // console.log("Tab 2");
-  // console.log(btn[1].dataset.tab);
-  div[1].classList.toggle('active');
-}
+// btn[0].onclick = function (event) {
+//   // console.log("Tab 1");
+//   // console.log(btn[0].dataset.tab);
+//   div[0].classList.toggle('active');
+// }
 
-btn[2].onclick = function (event) {
-  // console.log("Tab 3");
-  // console.log(btn[2].dataset.tab);
-  div[2].classList.toggle('active');
-}
+// btn[1].onclick = function (event) {
+//   // console.log("Tab 2");
+//   // console.log(btn[1].dataset.tab);
+//   div[1].classList.toggle('active');
+// }
 
-btn[3].onclick = function (event) {
-  // console.log("Tab 3");
-  // console.log(btn[2].dataset.tab);
-  for (let i = 0; i < div.length; i++) {
-    div[i].classList.remove('active');
+// btn[2].onclick = function (event) {
+//   // console.log("Tab 3");
+//   // console.log(btn[2].dataset.tab);
+//   div[2].classList.toggle('active');
+// }
+
+// btn[3].onclick = function (event) {
+//   // console.log("Tab 3");
+//   // console.log(btn[2].dataset.tab);
+//   for (let i = 0; i < div.length; i++) {
+//     div[i].classList.remove('active');
+//   }
+// }
+
+// Второй вариант. Вполе себе ок, наверное.
+
+let btn = document.querySelector('.buttonsHeader');
+let tabs = document.querySelectorAll('.tab');
+
+btn.onclick = function open(event) {
+  let button = event.target;
+  if (button.className === 'showButton') {
+    tabs.forEach(tab => {
+      if (tab.dataset.tab === button.dataset.tab) {
+        tab.classList.toggle('active');
+      } else {
+        tab.classList.remove('active');
+      }
+    });
   }
 }
