df2f44f94ae0a23a29b204876c0ccde06cbdb988
Vitalii Kopach <evilfossa@gmail.com>
2019-11-05 21:35:00 +0200

    Done.

--- JS Advanced/L2/classwork/js/task1.js
+++ JS Advanced/L2/classwork/js/task1.js
@@ -7,15 +7,15 @@
       6. Применить метод toggle на класс "Command"
     */
 
-var getById = document.getElementById("target")
+const getById = document.getElementById('target');
 
 function createDiv() {
-    let div = document.createElement('div');
-    div.className = "Ctrl Shift Delete";
-    div.classList.add("addClass");
-    div.classList.remove("Shift");
-    div.classList.toggle("Ctrl");
-    getById.appendChild(div);
+  const div = document.createElement('div');
+  div.className = 'Ctrl Shift Delete';
+  div.classList.add('addClass');
+  div.classList.remove('Shift');
+  div.classList.toggle('Ctrl');
+  getById.appendChild(div);
 }
 
 createDiv();
